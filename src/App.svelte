a00eafdc2afb17811e8e0758e44b4ae43fa9e156
Maximilian Becher <maximilian.becher@ueberbit.de>
2019-08-05 22:35:34 +0200

    Add ColorButton component
    * add global vars
    * custom checkbox styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,46 @@
 <script>
-	export let name;
+  import ColorButton from "./ColorButton.svelte";
+  export let name;
+
+  const colorFilterOptions = [
+    { id: "red", color: "red" },
+    { id: "red", color: "red" },
+    { id: "red", color: "red" },
+    { id: "red", color: "red" }
+  ];
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  :root {
+    --spacing-small: 8px;
+    --spacing: 16px;
+    --spacing-large: 34px;
+
+    --gry-light: lightgray;
+    --gry: gray;
+
+    --green-light: lightgreen;
+    --green: green;
+  }
+
+  h1 {
+    color: purple;
+  }
+
+  .colorButtons {
+    width: 200px;
+    display: flex;
+    justify-content: space-between;
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>Flagfinder</h1>
+
+<div class="colorButtons">
+  {#each colorFilterOptions as cButton}
+    <ColorButton {...cButton} />
+  {/each}
+
+</div>
+
+<div>Result goes here</div>
---
+++ src/ColorButton.svelte
@@ -0,0 +1,51 @@
+<script>
+  export let id = "text";
+  export let color;
+  export let selected;
+  export let disabled;
+</script>
+
+<style>
+  .colorButton {
+    position: relative;
+  }
+  input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    height: 0;
+    width: 0;
+  }
+
+  .colorButton {
+    box-sizing: border-box;
+    width: var(--spacing-large);
+    height: var(--spacing-large);
+    background: yellow;
+    border-radius: 50%;
+    border-color: var(--gry);
+    border-width: 2px;
+    border-style: solid;
+    cursor: pointer;
+  }
+
+  .colorButton.selected {
+    border-color: var(--green-light);
+    border-width: 3px;
+  }
+
+  .colorButton.disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+    background: var(--gry-light) !important;
+  }
+</style>
+
+<label
+  class="colorButton"
+  class:disabled
+  class:selected
+  title={id}
+  style="background: {color}">
+  <input type="checkbox" {disabled} bind:checked={selected} />
+</label>
