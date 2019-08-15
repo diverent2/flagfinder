e500e70dd25d249d09130683c6e88a3675ecca8d
Maximilian Becher <maximilian.becher@ueberbit.de>
2019-08-15 13:40:15 +0200

    Add some test data to flags.js [WIP]

--- src/data/flags.js
+++ src/data/flags.js
@@ -2,22 +2,47 @@ const flags = [
   {
     id: 'gay',
     name: 'gay pride flag',
-    colors: ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+    description: 'lorem ipsum',
+    colors: ['red', 'orange', 'yellow', 'green', 'blue', 'purple'],
+    image: {
+      source:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Gay_Pride_Flag.svg/2000px-Gay_Pride_Flag.svg.png',
+      alt: 'gay pride flag'
+    },
+    category: 'sexual'
   },
   {
     id: 'bi',
     name: 'bisexual pride flag',
-    colors: ['cyan', 'darkviolet', 'deepurple']
+    description: 'lorem ipsum',
+    colors: ['cyan', 'darkviolet', 'deepurple'],
+    image: {
+      source: 'url',
+      alt: 'gay pride flag'
+    },
+    category: 'sexual'
   },
   {
     id: 'trans',
     name: 'trans pride flag',
-    colors: ['lightpink', 'lightblue', 'white']
+    description: 'lorem ipsum',
+    colors: ['lightpink', 'lightblue', 'white'],
+    image: {
+      source: 'url',
+      alt: 'gay pride flag'
+    },
+    category: 'gender'
   },
   {
-    id: 'pan',
+    id: 'pansexual',
     name: 'pansexual pride flag',
-    colors: ['pink', 'lightblue', 'yellow']
+    description: 'lorem ipsum',
+    colors: ['pink', 'lightblue', 'yellow'],
+    image: {
+      source: 'url',
+      alt: 'gay pride flag'
+    },
+    category: 'sexual'
   }
 ];
 
