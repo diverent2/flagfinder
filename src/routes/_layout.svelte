f9548faf2cdefad39c196bec610dbc6917398f8a
Maximilian Becher <maximilian.becher@ueberbit.de>
2019-10-09 12:02:19 +0200

    Cleanup _layout and remove demo Nav

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,21 +1,13 @@
-<script>
-  import Nav from "../components/Nav.svelte";
-
-  export let segment;
-</script>
-
 <style>
   main {
+    width: 100vw;
+    height: auto;
+    min-height: 100vh;
     position: relative;
     margin: 0 auto;
     box-sizing: border-box;
-    height: 100vh;
   }
 </style>
-
-<!--
-<Nav {segment}/>
--->
 
 <main>
   <slot />
