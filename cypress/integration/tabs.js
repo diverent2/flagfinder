4b803f217a083702672aa866fbb87c1993091320
Maximilian Becher <maximilian.becher@ueberbit.de>
2019-11-08 00:19:30 +0100

    #7 Fix and optimise tab test
    The button click seems to be to quick to handle for cypress

--- cypress/integration/tabs.js
+++ cypress/integration/tabs.js
@@ -1,29 +1,40 @@
 describe('Detail Tabs', () => {
   beforeEach(() => {
     cy.visit('/flag/gay');
+    cy.viewport('iphone-6');
   });
 
   it('updates the active button style', () => {
     const active = 'tabButton--activeTab';
-    cy.get('.tabButtons > .tabButton:first-child').should('have.class', active);
-    cy.get('.tabButtons .tabButton:nth-child(2)')
+    cy.get('.tabButtons > .tabButton')
+      .first()
+      .should('have.class', active);
+
+    cy.get('.tabButtons > .tabButton')
+      .last()
+      .should('not.have.class', active)
+      .wait(200) // @TODO: figure out why this is required
       .click()
       .should('have.class', active);
-    cy.get('.tabButtons > .tabButton:first-child').should(
-      'not.have.class',
-      active
-    );
+
+    cy.get('.tabButtons .tabButton')
+      .first()
+      .should('not.have.class', active);
   });
 
   it('changes the active tab on click', () => {
-    cy.get('.tabButtons > .tabButton:first-child').then($button => {
-      const tabText = $button
-        .text()
-        .trim()
-        .toLowerCase();
-      cy.get('main > section').should('have.attr', 'title', tabText);
-    });
-    cy.get('.tabButtons > .tabButton:nth-child(2)')
+    cy.get('.tabButtons > .tabButton')
+      .first()
+      .then($button => {
+        const tabText = $button
+          .text()
+          .trim()
+          .toLowerCase();
+        cy.get('main > section').should('have.attr', 'title', tabText);
+      });
+
+    cy.get('.tabButtons > .tabButton:not(.tabButton--activeTab)')
+      .first()
       .click()
       .then($button => {
         const tabText = $button
