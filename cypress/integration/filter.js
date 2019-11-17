dfc472c1d3b21d482a969d74e6b8ce8d48df5a76
Maximilian Becher <maximilian.becher@ueberbit.de>
2019-11-17 23:11:06 +0100

    #16 Add test to check if filter remove  works
    Will select all types of filters and clear them afterwards.

--- cypress/integration/filter.js
+++ cypress/integration/filter.js
@@ -1,12 +1,31 @@
-describe("Searchfilter", () => {
+describe('Searchfilter', () => {
   beforeEach(() => {
-    cy.visit("/");
-    cy.get("a.button__search").click();
+    cy.visit('/');
+    cy.get('a.button__search').click();
   });
 
-  it("shows/hides remove filter button correctly", () => {
-    cy.get(".searchbox__delete").should("not.have.class", "visible");
-    cy.get("#search").type("g");
-    cy.get(".searchbox__delete").should("have.class", "visible");
+  it('shows/hides remove filter button correctly', () => {
+    cy.get('.searchbox__delete').should('not.have.class', 'visible');
+    cy.get('#search').type('g');
+    cy.get('.searchbox__delete').should('have.class', 'visible');
+  });
+
+  it('removes all filters through remove filter button', () => {
+    cy.get('.mainOptions .mainOptions__arrow--icon').click();
+    cy.get('#search').type('g');
+    cy.get('.colorButtons .button__color:nth-child(2)').click();
+    cy.get('.colorButtons .button__color:nth-child(5)').click();
+    cy.get('.filter-categories .button__category:nth-child(2)').click();
+    cy.get('.filter-categories .button__category:nth-child(4)').click();
+
+    cy.get('.searchbox__delete').click();
+
+    // got cleared
+    cy.get('#search').should('be.empty');
+    cy.get('.colorButtons .button__color.selected').should('have.length', 0);
+    cy.get('.filter-categories .button__category.selected').should(
+      'have.length',
+      0
+    );
   });
 });
