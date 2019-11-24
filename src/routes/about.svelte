4b0388c352a3af38199bf9502b8fe42dd2f42cd6
Maximilian Becher <maximilian.becher@ueberbit.de>
2019-11-25 00:20:37 +0100

    Turn header into a separate component

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,48 +1,11 @@
 <script>
   import Footer from "./../components/Footer.svelte";
+  import Header from "./../components/Header.svelte";
 </script>
 
 <style>
   .container {
     padding: 2rem;
-  }
-
-  header {
-    position: sticky;
-    top: 0;
-    width: 100vw;
-    padding: 1rem;
-    color: var(--white);
-    border-bottom-left-radius: 50%;
-    border-bottom-right-radius: 50%;
-    background: var(--blue);
-    text-align: center;
-    z-index: 1;
-
-    display: grid;
-    grid-template:
-      "back ." max-content
-      "title title" 1fr
-      / 1fr 1fr;
-  }
-
-  .header__goBack {
-    grid-area: back;
-    width: max-content;
-  }
-
-  .ico-arrowBack {
-    width: 1rem;
-    height: 1rem;
-    transition: color 0.2s ease;
-  }
-
-  .ico-arrowBack:hover {
-    color: var(--green-light);
-  }
-
-  h1 {
-    grid-area: title;
   }
 
   h2,
@@ -59,18 +22,7 @@
   <title>About | Prideflags.info</title>
 </svelte:head>
 
-<header>
-  <a href="./" class="header__goBack">
-    <svg
-      class="ico-arrowBack"
-      aria-label="Go back to search"
-      viewBox="0 0 32 32"
-      xmlns="http://www.w3.org/2000/svg">
-      <use href="icons/arrow-back.svg#arrow-back" />
-    </svg>
-  </a>
-  <h1>About</h1>
-</header>
+<Header>About</Header>
 
 <div class="container">
 
