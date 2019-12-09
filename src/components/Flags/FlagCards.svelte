e2aa35851307cf58bda5edd904de6a7583f7a6ba
Maximilian Becher <maximilian.becher@ueberbit.de>
2019-12-10 00:09:53 +0100

    Add FlagCards grid for desktop

--- src/components/Flags/FlagCards.svelte
+++ src/components/Flags/FlagCards.svelte
@@ -6,10 +6,21 @@
 
 <style>
   .flag-results {
-    margin: var(--spacing);
+    padding: var(--spacing);
     display: grid;
     grid-gap: var(--spacing);
     grid-template-columns: 100%; /*fix overflow*/
+    justify-content: center;
+
+    max-width: 1500px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  @media only screen and (min-width: 420px) {
+    .flag-results {
+      grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    }
   }
 </style>
 
