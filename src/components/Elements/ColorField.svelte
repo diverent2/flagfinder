ee5ef6018ef3633bf84ff2759f1c4dc6c1171872
Maximilian Becher <maximilian.becher@ueberbit.de>
2019-12-23 14:55:09 +0100

    #21 Add ColorField
    Combine color field with name and hue

---
+++ src/components/Elements/ColorField.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let name = "red";
+  export let hue = name;
+  export let color = "#e20004";
+</script>
+
+<style>
+  .colorField {
+    width: 4rem;
+  }
+
+  .colorField__color {
+    height: 2rem;
+    border-color: var(--black);
+    border-width: 1px;
+    border-style: solid;
+    box-shadow: var(--box-shadow);
+  }
+
+  .colorField__name {
+    font-size: 0.8rem;
+    line-height: 1;
+  }
+
+  .colorField__value {
+    font-size: 0.6rem;
+  }
+</style>
+
+<div class="colorField">
+  <div class="colorField__color" style="background: {color}" />
+  <div class="colorField__name">
+    {name}
+    {#if name.includes(hue) == false}
+      <span>({hue})</span>
+    {/if}
+  </div>
+  <div class="colorField__value">{color}</div>
+</div>
