8351c36450f67a8a6949273cfe7c0ab99c9c78d7
Maximilian Becher <maximilian.becher@ueberbit.de>
2020-01-04 18:01:38 +0100

    #23 Adjust FlagDetails/Header to new Navigation
    * Update back arrow size
    * Change background color
    * Move tabs out of grid

--- src/components/FlagDetails/Header.svelte
+++ src/components/FlagDetails/Header.svelte
@@ -9,12 +9,11 @@
   header {
     display: grid;
     grid-template:
-      "back ." max-content
-      "title flag" 1fr
-      "tabs tabs" auto
+      "back flag" max-content
+      "title ." 1fr
       / 1fr 1fr;
     width: 100vw;
-    padding: var(--spacing);
+    padding: var(--spacing-large) var(--spacing-xlarge);
 
     position: fixed;
     top: 0;
@@ -24,14 +23,19 @@
     border-bottom-right-radius: 25%;
     text-transform: capitalize;
     box-shadow: var(--box-shadow);
-    background: var(--blue);
+    background: var(--blue-dark-500);
     z-index: 1;
   }
 
   .flagDetails__goBack {
     grid-area: back;
-
     width: max-content;
+  }
+
+  .ico-arrowBack {
+    width: 1.5rem;
+    height: 1.5rem;
+    transition: color 0.2s ease;
   }
 
   .flagDetails__title {
@@ -45,30 +49,37 @@
     -webkit-box-orient: vertical;
   }
 
-  .ico-arrowBack {
-    width: 1rem;
-    height: 1rem;
-    transition: color 0.2s ease;
-  }
-
   .ico-arrowBack:hover {
     color: var(--green-light);
   }
 
   .flagDetails__headerImage {
     grid-area: flag;
+    position: absolute;
     width: auto;
-    height: 60px;
+    height: 80px;
     justify-self: end;
 
     border: var(--white) 2px solid;
   }
 
   .flagDetails__tabButtons {
-    grid-area: tabs;
-    width: 60vw;
     position: absolute;
+    bottom: -1rem;
+    width: 60vw;
     justify-self: center;
+  }
+
+  @media only screen and (min-width: 650px) and (min-height: 450px) {
+    header {
+      width: calc(100vw - 150px);
+      margin-left: 150px;
+
+      padding-top: var(--spacing-large);
+      padding-bottom: var(--spacing-large);
+      padding-left: 4rem;
+      padding-right: 3rem;
+    }
   }
 </style>
 
