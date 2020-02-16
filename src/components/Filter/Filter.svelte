01884895ab95ed4eafa04806cf32874a2ced151a
Maximilian Becher <maximilian.becher@ueberbit.de>
2020-02-16 04:25:37 +0100

    #32 Turn category filters into actual buttons
    Improve UX-readability for the user.
    
    * cleanup fitlers

--- src/components/Filter/Filter.svelte
+++ src/components/Filter/Filter.svelte
@@ -17,6 +17,7 @@
 <script>
   /*Data*/
   import { filterColors, filterCategories } from "../../data/_filter";
+  import IconButton from "../Elements/IconButton.svelte";
   import ColorSpot from "../Elements/ColorSpot.svelte";
 
   export let activeColorFilters;
@@ -26,23 +27,18 @@
   $: activeCategoryFilters;
 </script>
 
-<style>
+<style lang="scss">
   fieldset {
     color: var(--white);
     margin: 0;
     border: none;
-  }
-  .filter {
-    display: flex;
-    user-select: none;
   }
 
   .filter-color {
     flex-grow: 3;
   }
 
-  .colorButtons,
-  .categoryButtons {
+  .colorButtons {
     display: grid;
     grid-template-columns: repeat(auto-fill, var(--spacing-xlarge));
     grid-gap: 0.5rem;
@@ -56,71 +52,125 @@
     width: 0;
   }
 
-  .button__color,
-  .button__category {
-    position: relative;
-  }
-
-  .button {
+  .colorButton {
     width: var(--spacing-xlarge);
     height: var(--spacing-xlarge);
-    background: var(--white);
-    border-radius: 50%;
-    border-width: 2px;
-    border-style: solid;
-    cursor: pointer;
-    box-shadow: var(--box-shadow);
-  }
+    position: relative;
 
-  .button__color,
-  .button__category {
     display: flex;
     justify-content: center;
     align-items: center;
+
+    //not selected
+    border-color: var(--gry-light);
+    border-radius: 50%;
+    border-width: 2px;
+    border-style: solid;
+
+    cursor: pointer;
+    background: var(--white);
+    box-shadow: var(--box-shadow);
     transition: border-color 0.2s ease;
+
+    .colorButton_checkmark {
+      width: 1rem;
+      height: 1.1rem;
+      position: absolute;
+      right: -4px;
+      bottom: 0px;
+      color: var(--green-light);
+      transition: opacity 0.2s ease;
+      opacity: 0;
+    }
+
+    &.selected {
+      border-color: var(--green-light);
+
+      .colorButton_checkmark {
+        opacity: 1;
+      }
+    }
   }
 
-  .buttonColor__checkmark {
-    width: 1rem;
-    height: 1.1rem;
-    position: absolute;
-    right: -4px;
-    bottom: 0px;
-    color: var(--green-light);
-    transition: opacity 0.2s ease;
-    opacity: 0;
+  .categoryButtons {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: var(--spacing-small);
   }
 
-  .button__color.selected {
-    border-color: var(--green-light);
-  }
+  .categoryButton {
+    height: 100%;
+    max-width: 100px;
+    padding: var(--spacing-tiny) var(--spacing-small);
+    color: var(--white);
 
-  .ico-category {
-    width: 1rem;
-    height: 1rem;
-  }
+    border: var(--gry-light) 1px solid;
+    border-radius: 35px;
 
-  .button__color.selected .buttonColor__checkmark {
-    opacity: 1;
-  }
+    cursor: pointer;
+    user-select: none;
 
-  .button:not(.selected) {
-    border-color: var(--gry-light) !important;
-  }
+    .categoryButton_inner {
+      display: flex;
+      align-items: center;
 
-  .button__category:not(.selected) .ico-category {
-    color: var(--gry-light) !important;
+      margin: 0 auto;
+      width: max-content;
+    }
+
+    .categoryButton_icon {
+      width: 1rem;
+      height: 1rem;
+    }
+
+    .categoryButton_text {
+      margin-left: var(--spacing-small);
+    }
+
+    &.selected {
+      color: var(--black);
+      border: transparent 1px solid;
+      background: var(--color);
+    }
   }
 </style>
 
 <div class="filter">
 
+  <fieldset class="filter-categories">
+    <legend>Categories</legend>
+    <div class="categoryButtons">
+      {#each filterCategories as category}
+        <label
+          title={category.id}
+          class="categoryButton"
+          style="--color: {category.color};"
+          class:selected={category.selected}>
+          <div class="categoryButton_inner">
+            <input
+              type="checkbox"
+              value={category.id}
+              bind:checked={category.selected}
+              bind:group={activeCategoryFilters} />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              class="categoryButton_icon">
+              <use href="icons/{category.id}.svg#{category.id}" />
+            </svg>
+            <span class="categoryButton_text">{category.id}</span>
+          </div>
+        </label>
+      {/each}
+    </div>
+  </fieldset>
+
   <fieldset class="filter-color">
-    <legend>colors</legend>
+    <legend>Colors</legend>
     <div class="colorButtons">
       {#each filterColors as colorFilter}
         <label
-          class="button button__color"
+          class="colorButton"
           title={colorFilter.id}
           class:selected={colorFilter.selected}>
 
@@ -134,40 +184,11 @@
             value={colorFilter.id}
             bind:group={activeColorFilters}
             bind:checked={colorFilter.selected} />
-          <div class="buttonColor__checkmark">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              class="ico-checkmark">
+          <div class="colorButton_checkmark">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
               <use href="icons/checkmark-circle.svg#checkmark-circle" />
             </svg>
           </div>
-        </label>
-      {/each}
-    </div>
-  </fieldset>
-
-  <fieldset class="filter-categories">
-    <legend>categories</legend>
-    <div class="categoryButtons">
-      {#each filterCategories as category}
-        <label
-          class="button button__category"
-          title={category.id}
-          class:selected={category.selected}
-          style="border-color: {category.color}">
-          <input
-            type="checkbox"
-            value={category.id}
-            bind:checked={category.selected}
-            bind:group={activeCategoryFilters} />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            class="ico-category"
-            style="color: {category.color}">
-            <use href="icons/{category.id}.svg#{category.id}" />
-          </svg>
         </label>
       {/each}
     </div>
