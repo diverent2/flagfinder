3abc9e6098874fa43d3dc115701ee2b2e043b745
Maxi Becher <diverent2@gmail.com>
2020-06-01 20:58:29 +0200

    📦 NEW: Add placeholder pages for settings and bookmarks

---
+++ src/routes/bookmarks.svelte
@@ -0,0 +1,50 @@
+<script>
+  import { app_name, app_baseUrl } from "../data/global.js";
+
+  import Metainfos from "./../components/Helpers/Metainfos.svelte";
+  import Header from "./../components/Header.svelte";
+
+  const meta = {
+    title: `Bookmarks | ${app_name}`,
+    desc: `See all of your favortite prideflags in one place!`,
+    url: `${app_baseUrl}/bookmarks`,
+    image: `${app_baseUrl}/socialcards/search.png`
+  };
+</script>
+
+<style>
+  .anchor {
+    scroll-margin-top: 120px;
+  }
+
+  h2,
+  h3 {
+    margin-top: 0.5rem;
+    text-decoration-line: underline;
+    text-transform: capitalize;
+  }
+
+  h2 {
+    text-decoration-thickness: 3px;
+  }
+
+  h3 {
+    text-decoration-thickness: 1px;
+  }
+
+  section {
+    margin-bottom: 3rem;
+  }
+
+  section:last-of-type {
+    margin-bottom: 0;
+  }
+</style>
+
+<Metainfos {...meta} />
+
+<Header backLink="/">Bookmarks</Header>
+
+<div class="container container-padding-top">
+  <h2 class="text--center">Coming soon</h2>
+</div>
