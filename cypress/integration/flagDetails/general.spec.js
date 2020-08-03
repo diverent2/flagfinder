6659cc8b2149fc8cc76450223dbae812ec279756
Maxi Becher <diverent2@gmail.com>
2020-08-03 22:16:50 +0200

    🐛 FIX: #64 Use og:image png fallback for flag svg images

---
+++ cypress/integration/flagDetails/general.spec.js
@@ -0,0 +1,28 @@
+describe('flag details have the correct meta data', () => {
+  it('always takes a png as og:image', () => {
+    cy.visit('/flag/gay');
+    cy.get('[data-cy-flag-header-image]').should(
+      'have.attr',
+      'src',
+      'flags/gay/gay.png'
+    );
+    cy.get('head meta[property="og:image"]').should(
+      'have.attr',
+      'content',
+      'https://prideflags.info/flags/gay/gay.png'
+    );
+
+    cy.log('flag image is an svg');
+    cy.visit('/flag/omnisexual');
+    cy.get('[data-cy-flag-header-image]').should(
+      'have.attr',
+      'src',
+      'flags/omnisexual.svg'
+    );
+    cy.get('head meta[property="og:image"]').should(
+      'have.attr',
+      'content',
+      'https://prideflags.info/flags/omnisexual.png'
+    );
+  });
+});
