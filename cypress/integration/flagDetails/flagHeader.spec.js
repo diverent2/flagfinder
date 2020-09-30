27970146218b9bd4772297ee42a669b1a29bccb5
Maxi Becher <diverent2@gmail.com>
2020-09-30 13:30:33 +0200

    🐛 FIX: flagHeader test (after cypress update)

--- cypress/integration/flagDetails/flagHeader.spec.js
+++ cypress/integration/flagDetails/flagHeader.spec.js
@@ -5,27 +5,26 @@ describe('Header for flag info', () => {
   });
 
   it('updates headerstate on scroll', () => {
-    cy.wrap([0, 4, 10, 21]).each((positionOfCard, index) => {
-      const headerHeightThreshold = 200;
-      cy.visit('/');
-      cy.get('[data-cy-flagcard]').eq(positionOfCard).scrollIntoView().click();
-      cy.get('[data-cy-flag-header]').as('header');
-      cy.get('@header').should('have.attr', 'data-state', 'full');
-      cy.get('@header')
-        .invoke('outerHeight')
-        .should('be.above', headerHeightThreshold);
-      cy.scrollTo(0, 100);
-      cy.wait(100);
-      cy.get('@header').should('have.attr', 'data-state', 'collapsed');
-      cy.get('@header')
-        .invoke('outerHeight')
-        .should('be.below', headerHeightThreshold);
-      cy.scrollTo(0, 10);
-      cy.wait(100);
-      cy.get('@header').should('have.attr', 'data-state', 'full');
-      cy.get('@header')
-        .invoke('outerHeight')
-        .should('be.above', headerHeightThreshold);
-    });
+    const headerHeightThreshold = 200;
+    cy.wrap(['progressive', 'gay', 'lesbian', 'rubber']).each(
+      (flagname, index) => {
+        cy.visit(`/flag/${flagname}`);
+        cy.get('[data-cy-flag-header]').as('header');
+        cy.get('@header').should('have.attr', 'data-state', 'full');
+        cy.get('@header')
+          .invoke('outerHeight')
+          .should('be.above', headerHeightThreshold);
+        cy.scrollTo(0, 100);
+        cy.get('@header').should('have.attr', 'data-state', 'collapsed');
+        cy.get('@header')
+          .invoke('outerHeight')
+          .should('be.below', headerHeightThreshold);
+        cy.scrollTo(0, 10);
+        cy.get('@header').should('have.attr', 'data-state', 'full');
+        cy.get('@header')
+          .invoke('outerHeight')
+          .should('be.above', headerHeightThreshold);
+      }
+    );
   });
 });
