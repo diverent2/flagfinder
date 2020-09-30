67c5f4144c675260f1f0d7ea25670224fb069fd7
Maxi Becher <diverent2@gmail.com>
2020-09-30 14:08:19 +0200

    📦 NEW: Setup precy visual regression testing

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,31 +1,8 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+// Reference: https://on.cypress.io/custom-commands
+
+import '@percy/cypress';
 
 Cypress.Commands.add('filloutFormElement', (selector, content, position) => {
-
   switch (position) {
     case undefined:
       cy.get(selector).type(content);
