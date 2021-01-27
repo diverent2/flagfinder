ba09318e8cc825bcde1fb9964230ab82197f2bab
Maxi Becher <diverent2@gmail.com>
2021-01-27 18:30:00 +0100

    👌 IMPROVE: Optimize navigation spec
    * split tests

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,60 +1,87 @@
 describe('the navigation menu shows the right context', () => {
-  beforeEach(() => {
-    cy.viewport('iphone-6');
+
+  describe('shows/hides main nav on search for mobile/desktop', () => {
+  
+    beforeEach(() => {
+      cy.visit('/');
+    })
+
+    it('shows/hides main nav on search for mobile', () => {
+      cy.viewport('iphone-6');
+      cy.get('[data-cy-footernav-option="footer"]').should('not.exist');
+      cy.get('[data-cy-footernav-option="bookmark"]').should('not.exist');
+      cy.get('[data-cy-footernav-option="share"]').should('not.exist');
+      cy.get('[data-cy-footernav-option="settings"]')
+        .should('exist')
+        .and('be.visible');
+      cy.get('[data-cy-footernav-option="about"]')
+        .should('exist')
+        .and('be.visible');
+    });
+
+        
+    it('shows/hides main nav on search for desktop', () => {
+      cy.viewport('macbook-11');
+      cy.get('[data-cy-footernav-option="footer"]').should('not.exist');
+      cy.get('[data-cy-footernav-option="bookmark"]').should('not.exist');
+      cy.get('[data-cy-footernav-option="share"]').should('not.exist');
+      cy.get('[data-cy-footernav-option="settings"]').should('not.be.visible');
+      cy.get('[data-cy-footernav-option="about"]').should('not.be.visible');
+      cy.percySnapshot('navigation--search');
+    });
+  
   });
 
-  it('shows/hides main nav on search for mobile/desktop', () => {
-    cy.visit('/');
-    cy.get('[data-cy-footernav-option="footer"]').should('not.exist');
-    cy.get('[data-cy-footernav-option="bookmark"]').should('not.exist');
-    cy.get('[data-cy-footernav-option="share"]').should('not.exist');
-    cy.get('[data-cy-footernav-option="settings"]')
-      .should('exist')
-      .and('be.visible');
-    cy.get('[data-cy-footernav-option="about"]')
-      .should('exist')
-      .and('be.visible');
+  describe('shows flagcontext nav on /flag for mobile and desktop', () => {
+    beforeEach(() => {
+      cy.visit('/flag/gay');
+      cy.wait(100);
+    });
 
-    cy.viewport('macbook-11');
-    cy.get('[data-cy-footernav-option="footer"]').should('not.exist');
-    cy.get('[data-cy-footernav-option="bookmark"]').should('not.exist');
-    cy.get('[data-cy-footernav-option="share"]').should('not.exist');
-    cy.get('[data-cy-footernav-option="settings"]').should('not.be.visible');
-    cy.get('[data-cy-footernav-option="about"]').should('not.be.visible');
-    cy.percySnapshot('navigation--search');
-  });
+    it('shows flagcontext nav on /flag for mobile', () => {
+      cy.viewport('iphone-6');
+      cy.get('[data-cy-footernav-option="footer"]').should('not.exist');
+      cy.get('[data-cy-footernav-option="bookmark"]').should('exist');
+      cy.get('[data-cy-footernav-option="share"]').should('exist');
+      cy.get('[data-cy-footernav-option="settings"]').should('not.exist');
+      cy.get('[data-cy-footernav-option="about"]').should('not.exist');
+    });
+    
+    it('shows flagcontext nav on /flag for desktop', () => {
+      cy.viewport('macbook-11');
+      cy.get('[data-cy-footernav-option="footer"]').should('not.exist');
+      cy.get('[data-cy-footernav-option="bookmark"]').should('exist');
+      cy.get('[data-cy-footernav-option="share"]').should('exist');
+      cy.get('[data-cy-footernav-option="settings"]').should('not.exist');
+      cy.get('[data-cy-footernav-option="about"]').should('not.exist');
+      cy.percySnapshot('navigation--flag-gay');
+    });
+  })
 
-  it('shows flagcontext nav on /flag for mobile and desktop', () => {
-    cy.visit('/flag/gay');
-    cy.get('[data-cy-footernav-option="footer"]').should('not.exist');
-    cy.get('[data-cy-footernav-option="bookmark"]').should('exist');
-    cy.get('[data-cy-footernav-option="share"]').should('exist');
-    cy.get('[data-cy-footernav-option="settings"]').should('not.exist');
-    cy.get('[data-cy-footernav-option="about"]').should('not.exist');
+  describe('shows/hides footertext on /about for mobile/desktop', () => {
 
-    cy.viewport('macbook-11');
-    cy.get('[data-cy-footernav-option="footer"]').should('not.exist');
-    cy.get('[data-cy-footernav-option="bookmark"]').should('exist');
-    cy.get('[data-cy-footernav-option="share"]').should('exist');
-    cy.get('[data-cy-footernav-option="settings"]').should('not.exist');
-    cy.get('[data-cy-footernav-option="about"]').should('not.exist');
-    cy.percySnapshot('navigation--flag-gay');
-  });
+    beforeEach(() => {
+      cy.visit('/about');
+      cy.wait(100);
+    });
 
-  it('shows/hides footertext on /about for mobile/desktop', () => {
-    cy.visit('/about');
-    cy.get('[data-cy-footernav-option="footer"]').should('exist');
-    cy.get('[data-cy-footernav-option="bookmark"]').should('not.exist');
-    cy.get('[data-cy-footernav-option="share"]').should('not.exist');
-    cy.get('[data-cy-footernav-option="options"]').should('not.exist');
-    cy.get('[data-cy-footernav-option="about"]').should('not.exist');
+    it('shows/hides footertext on /about for mobile', () => {
+      cy.viewport('iphone-6');
+      cy.get('[data-cy-footernav-option="footer"]').should('exist');
+      cy.get('[data-cy-footernav-option="bookmark"]').should('not.exist');
+      cy.get('[data-cy-footernav-option="share"]').should('not.exist');
+      cy.get('[data-cy-footernav-option="options"]').should('not.exist');
+      cy.get('[data-cy-footernav-option="about"]').should('not.exist');
+    })
 
-    cy.viewport('macbook-11');
-    cy.get('[data-cy-footernav-option="footer"]').should('exist');
-    cy.get('[data-cy-footernav-option="bookmark"]').should('not.exist');
-    cy.get('[data-cy-footernav-option="share"]').should('not.exist');
-    cy.get('[data-cy-footernav-option="options"]').should('not.exist');
-    cy.get('[data-cy-footernav-option="about"]').should('not.exist');
-    cy.percySnapshot('navigation--about');
+    it('shows/hides footertext on /about for desktop', () => {
+      cy.viewport('macbook-11');
+      cy.get('[data-cy-footernav-option="footer"]').should('exist');
+      cy.get('[data-cy-footernav-option="bookmark"]').should('not.exist');
+      cy.get('[data-cy-footernav-option="share"]').should('not.exist');
+      cy.get('[data-cy-footernav-option="options"]').should('not.exist');
+      cy.get('[data-cy-footernav-option="about"]').should('not.exist');
+      cy.percySnapshot('navigation--about');
+    });
   });
 });
